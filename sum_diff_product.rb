dba9a96adb65c83cecd6857fd0ce890600022c2e
Juan Ontiveros <jontiveros@chicagotechacademy.org>
2013-11-15 15:03:24 -0600

    new commit

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,11 +1,29 @@
-puts "add two numbers"
-def method_name (a , b)
+puts "enter one number"
+	one_number = gets.chomp 
+puts "enter another number"	
+	two_number = gets.chomp
 
-gets.chomp = a
-	
-	s = a + b
-	d = a - b
-	p = a * b
+class Number
+attr_accessor'one_number', 'two_number'	
 
-	puts"#{s} = sum, #{d} = diff, #{p} = product"
-end
+	def initialize(one_number, two_number)
+    	@one_number = one_number
+    	@two_number = two_number
+	  end
+
+  	def answerAdd
+ 		@one_number.to_i + @two_number.to_i
+	end
+	def answerDiff
+		@one_number.to_i - @two_number.to_i
+	end
+	def answerPro
+		@one_number.to_i * @two_number.to_i
+	end
+end 	
+
+Money = Number.new one_number,two_number
+
+puts Money.answerAdd
+puts Money.answerDiff
+puts Money.answerPro
